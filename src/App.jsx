ecbc0698d39b88f5a95e9d170db519b0da1c82a5
mjohanbintangp <m.johanbintang.pratama@gmail.com>
2026-01-29 14:33:29 +0700

    clear project

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QuizProvider } from "./store/QuizProvider";
 import Quiz from "./pages/Quiz";
+import Result from "./pages/Result";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <QuizProvider>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Quiz />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/quiz" element={<Quiz />} />
+          <Route path="/result" element={<Result />} />
         </Routes>
       </BrowserRouter>
     </QuizProvider>
--- src/services/triviaService.js
+++
@@ -1,5 +0,0 @@
-export const fetchQuestions = async (amount = 10) => {
-  const res = await fetch(`https://opentdb.com/api.php?${amount}}`);
-  const data = await res.json();
-  return data.results;
-};
